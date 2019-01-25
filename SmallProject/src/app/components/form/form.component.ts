fcc147ec7018338381cc5e5017857a9c83ba00b2
Sharma <212725513@logon.ds.ge.com>
2019-01-25 16:53:00 +0530

    Form is incomplete - data not coming in UI

---
+++ SmallProject/src/app/components/form/form.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+class Form{
+  id : number;
+  firstname : string;
+  lastname : string;
+}
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+export class FormComponent  {
+
+  form : Form[]; //declaration
+
+  submit(form_p: Form[]) {
+        this.form = form_p;
+    console.log("Pratishtha");
+    console.log(this.form);
+  }
+
+}
